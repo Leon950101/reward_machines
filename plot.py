2a5a7f2665d42af1ad0e93b14e240d7a0d9e5999
Yang Li <yali00004@stud.uni-saarland.de>
2022-07-06 15:35:01 +0200

    plot file

---
+++ plot.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+algorithms = ['crm', 'crm-rs', 'hrm', 'hrm-rs', 'ql', 'ql-rs']
+plot_index = 4
+for i in range(20):
+    df = pd.read_csv('my_results_baseline/' + algorithms[plot_index] +'/office-single/M1/' + str(i) + '/progress.csv')
+    df = df[{'steps', 'total reward'}]
+    df.rename(columns = {'total reward':str(i)}, inplace = True)
+    if i == 0:
+        df_last = df
+    else:
+        df_last = pd.merge(df_last, df, on='steps')
+df_last = df_last.drop(columns = ['steps'])
+steps = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] # thousands
+df_last['mean'] = df_last.mean(axis = 1)
+df_last['std'] = df_last.std(axis = 1)
+df_last['x'] = pd.DataFrame(data=steps)
+print(df_last)
+fig, ax = plt.subplots()
+plt.plot(df_last['x'], df_last['mean'], label=algorithms[plot_index])
+plt.fill_between(df_last['x'], df_last['mean'] + df_last['std'], df_last['mean'] - df_last['std'], alpha=0.2)
+# plt.set_title(algorithms[plot_index])
+plt.legend()
+plt.show()
