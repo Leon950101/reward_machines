4fc39744596ae67bf739f456e2942fa53d02436c
Yang Li <yali00004@stud.uni-saarland.de>
2022-07-26 16:13:53 +0200

    clean

--- reward_machines/plot.py
+++ reward_machines/plot.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-algorithms = ['crm', 'crm-rs', 'hrm', 'hrm-rs', 'ql', 'ql-rs']
+algorithms = ['hrm', 'hrm-rs', 'hrm-rs-wpk', 'hrm-wpk' ] # ['crm', 'crm-rs', 'hrm', 'hrm-rs', 'ql', 'ql-rs'] # 
 # plot_index = 4
 for plot_index in range(len(algorithms)):
-    for i in range(20):
-        df = pd.read_csv('../../my_results/' + algorithms[plot_index] +'/office-single/M1/' + str(i) + '/progress.csv')
+    for i in range(10):
+        df = pd.read_csv('../my_results_pk/' + algorithms[plot_index] +'/office-single/M1/' + str(i) + '/progress.csv')
         df = df[{'steps', 'total reward'}]
         df.rename(columns = {'total reward':str(i)}, inplace = True)
         if i == 0:
             df_last = df
         else:
             df_last = pd.merge(df_last, df, on='steps')
+    steps = df_last['steps'].copy()
     df_last = df_last.drop(columns = ['steps'])
-    steps = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] # thousands
     df_last['mean'] = df_last.mean(axis = 1)
     df_last['std'] = df_last.std(axis = 1)
     df_last['x'] = pd.DataFrame(data=steps)
--- reward_machines/run_office_pk.sh
+++ reward_machines/run_office_pk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../reward_machines
-for i in `seq 0 19`; # 59 
+for i in `seq 0 9`; # 59 
 do
 	# Office Single Task
 	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --use_self_loops --log_path=../my_results_pk/hrm-wpk/office-single/M1/$i
