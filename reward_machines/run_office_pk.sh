33b9f7597868e2e08787e61515addad93fc373a3
Yang Li <yali00004@stud.uni-saarland.de>
2022-07-26 21:11:05 +0200

    pk

--- reward_machines/run_office_pk.sh
+++ reward_machines/run_office_pk.sh
@@ -3,7 +3,7 @@ cd ../reward_machines
 for i in `seq 0 9`; # 59 
 do
 	# Office Single Task
-	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --use_self_loops --log_path=../my_results_pk/hrm-wpk/office-single/M1/$i
-	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --use_self_loops --log_path=../my_results_pk/hrm-rs-wpk/office-single/M1/$i --use_rs
+	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --use_self_loops --log_path=../../results/my_results_pk/hrm-wpk/office-single/M1/$i
+	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --use_self_loops --log_path=../../results/my_results_pk/hrm-rs-wpk/office-single/M1/$i --use_rs
 
 done
