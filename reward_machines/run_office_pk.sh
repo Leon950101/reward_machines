27df1505138a00b539bffdded2300907c74b0a22
Yang Li <yali00004@stud.uni-saarland.de>
2022-07-26 15:31:21 +0200

    nip

--- reward_machines/run_office_pk.sh
+++ reward_machines/run_office_pk.sh
@@ -4,8 +4,8 @@ for i in `seq 0 19`; # 59
 do
 	# Office Single Task
 	# python run.py --alg=qlearning --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --log_path=../my_results_pk/ql/office-single/M1/$i
-	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --log_path=../my_results_pk/hrm/office-single/M1/$i
+	# python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --log_path=../my_results_pk/hrm/office-single/M1/$i
 	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --use_self_loops=True --log_path=../my_results_pk/hrm-wpk/office-single/M1/$i
-	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --log_path=../my_results_pk/hrm-rs/office-single/M1/$i --use_rs
+	# python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --log_path=../my_results_pk/hrm-rs/office-single/M1/$i --use_rs
 	python run.py --alg=hrm --env=Office-single-v0 --num_timesteps=1e5 --gamma=0.9 --use_self_loops=True --log_path=../my_results_pk/hrm-rs-wpk/office-single/M1/$i --use_rs
 done
